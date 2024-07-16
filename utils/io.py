15dc6e7caf2524d01381793321696c2a58408f05
Généreux AKOTENOU <genereux.akotenou@gmail.com>
2024-07-16 03:36:15 +0100

    load data

--- utils/io.py
+++ utils/io.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
 class ENoseIO:
-    def read_patient_data(filepath):
+    def read_patient_data(filepath, is_labeled=True):
         with open(filepath, 'r') as file:
             lines = file.readlines()
             patient_id = int(lines[0].split(':')[1].strip())
-            result = lines[1].split(':')[1].strip()
+
             data = pd.read_csv(filepath, sep='\t', skiprows=3)
+            if is_labeled:
+                result = lines[1].split(':')[1].strip()
+                data['Result'] = result
+            
             data['Patient_ID'] = patient_id
-            data['Result'] = result
+
         return data
